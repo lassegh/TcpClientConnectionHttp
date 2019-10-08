e715be28e199bf5c39b7e0a612547d16dd3d0208
Lasse Grønbech <lasse.groenbech@gmail.com>
2019-10-08 21:42:20 +0200

    Initial commit

---
+++ TcpClientConnectionHttp/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TcpClientConnectionHttp
+{
+    public class Car
+    {
+        public string Vendor { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ TcpClientConnectionHttp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace TcpClientConnectionHttp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Client client = new Client();
+            Car car = new Car()
+            {
+                Model = "A5",
+                Vendor = "Audi",
+                Price = 800000
+            };
+            client.HttpPostRequestAsync(car);
+            Thread.Sleep(10000);
+            Console.WriteLine(client.HttpGetAllRequestAsync().Result);
+            Console.ReadKey();
+        }
+    }
+}
